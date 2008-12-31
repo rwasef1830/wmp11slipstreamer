1c5a0de80b2881d822bd6fd27f7e9d8c2b44f680
epsilon <epsilon@localhost>
2008-12-31 21:54:03 +0000

    Rewrote translation build script so that it can accommodate many resource files in the same assembly. Cleaned up translation files, need new ones.

---
+++ Translations/MakeWMP11Translations.cmd
@@ -0,0 +1,37 @@
+@Echo off
+:start
+setlocal enableextensions
+goto setvars
+
+:setvars
+set companyName=Boooggy and n7Epsilon
+set strongNameKey=..\WMP11Slipstreamer\WMP11SlipstreamerKey.snk
+set assemblyName=WMP11Slipstreamer
+set assemblyVersion=1.1.0.0
+set tempGenPath=%Temp%\%~n0%Random%.tmp
+If exist "%tempGenPath%" rd /s /q "%tempGenPath%"
+FOR /F "usebackq tokens=5" %%i IN (`dir .\Source ^| FIND "<DIR>"`) DO (
+	If not "%%i"=="." If not "%%i"==".." CALL :genResource "%%i"
+)
+If exist "%tempGenPath%" rd /s /q "%tempGenPath%"
+goto end
+
+:genResource
+set currCulturePath=%~dp0Source\%~1
+set currCulture=%~n1
+set currOutputDir=%~dp0Output\%currCulture%
+If not exist "%tempGenPath%" mkdir "%tempGenPath%"
+If exist "%currOutputDir%" rd /s /q "%currOutputDir%"
+mkdir "%currOutputDir%"
+set currResDescPath=%tempGenPath%\resDesc.txt
+del /Q "%currResDescPath%" 1>NUL 2>NUL 
+(Echo %currCulturePath%\ParsersMsg.resx/Epsilon.n7Framework.Epsilon.Parsers/ParsersMsg
+Echo %currCulturePath%\SlipstreamersMsg.resx/Epsilon.n7Framework.Epsilon.Slipstreamers/SlipstreamersMsg
+Echo %currCulturePath%\Msg.resx/Epsilon.WMP11Slipstreamer.Localization/Msg
+) >> "%currResDescPath%"
+CALL MakeSatelliteDll.cmd "%currResDescPath%" "%strongNameKey%" "%assemblyName%" "%assemblyVersion%" "%currCulture%" "%companyName%" "%currOutputDir%"
+del /Q "%currResDescPath%" 1>NUL
+goto :EOF
+
+:end
+endlocal
