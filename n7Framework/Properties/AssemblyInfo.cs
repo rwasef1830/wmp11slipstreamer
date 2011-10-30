ceb4a40944340e5d07c0dcfee2b1b74636890003
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2011-10-30 22:52:21 +0200

    - Add hooks with mercurial and automatic versioning. - Fixup build output folder.

--- n7Framework/Properties/AssemblyInfo.cs
+++ n7Framework/Properties/AssemblyInfo.cs
@@ -22,16 +22,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("b12e315c-bb77-4e23-b2f7-497a02d6470a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
